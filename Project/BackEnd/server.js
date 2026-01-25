5993eda6841634e79b4ebd56163d5ea98e2fad7b
Jayasurya <61399363+jayasurya-developer@users.noreply.github.com>
2026-01-25 21:35:11 +0530

    update

--- Project/BackEnd/server.js
+++ Project/BackEnd/server.js
@@ -9,12 +9,11 @@ const cors = require("cors");
 // server port
 const PORT = process.env.PORT || 5000;
 const Auth = require("./routes/Auth")
-app.use(cors(
-    {
-        origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-        credentials: true
-    }
-));
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true
+}));
 app.use(express.json())
 // Connect Database 
 connectDB();
