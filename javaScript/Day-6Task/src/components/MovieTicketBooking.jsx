8c231035778f91c119e81413311dcd300e1ebbf7
Jayasurya <61399363+jayasurya-developer@users.noreply.github.com>
2026-06-24 17:56:02 +0530

    update

---
+++ javaScript/Day-6Task/src/components/MovieTicketBooking.jsx
@@ -0,0 +1,18 @@
+// Task 3: Movie Ticket Booking Requirements: Display movie list Select
+//  movie Select seats Calculate total amount Show booking summary
+
+import React from 'react'
+
+function MovieTicketBooking() {
+
+    8
+
+  return (
+    <div>
+       
+      
+    </div>
+  )
+}
+
+export default MovieTicketBooking
