a41d7f5e09a00f3939eb0abc30c4e87ebe3df063
Jayasurya <61399363+jayasurya-developer@users.noreply.github.com>
2026-07-22 12:33:39 +0530

    update

--- javaScript/Day-6Task/src/App.jsx
+++ javaScript/Day-6Task/src/App.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import OnlineExamResult from './components/OnlineExamResult'
 
 function App() {
+
+  const outputRef = useRef(0)
+const [value, setValue] = useState(0)
+  function handleClick(){
+    const result = outputRef.current + 1
+    console.log(result)
+  }
+
+  function hanldeUpdate(){
+    setValue(outputRef.current)
+  }
   return (
     <div>
-      <OnlineExamResult/>
-      
+
+      {outputRef.current}
+      <button onClick={handleClick}>click</button>
+      <button onClick={hanldeUpdate}>update</button>
+      {/* <OnlineExamResult/> */}
     </div>
   )
 }
--- javaScript/Day-6Task/src/components/MovieTicketBooking.jsx
+++ javaScript/Day-6Task/src/components/MovieTicketBooking.jsx
@@ -5,7 +5,6 @@ import React from 'react'
 
 function MovieTicketBooking() {
 
-    8
 
   return (
     <div>
