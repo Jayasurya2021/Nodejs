b343a0a87cd2150de6aa3ed6cad5c34798816910
Jayasurya <61399363+jayasurya-developer@users.noreply.github.com>
2026-08-06 18:26:31 +0530

    js compoeenetns like create a pratice

--- js-compoenents/card.js
+++ js-compoenents/card.js
@@ -2,11 +2,28 @@
 export function card(container, carddata) {
     container.innerHTML = carddata.map((e) => (`
   <div class="card">
+    <img src="${e.thumbnail}" alt="${e.title}">
+
     <h2>${e.title}</h2>
+
     <p>${e.description}</p>
-    <h3>${e.price}</h3>
+
+    <p><strong>Category:</strong> ${e.category}</p>
+
+    <p><strong>Brand:</strong> ${e.brand}</p>
+
+    <p><strong>Price:</strong> $${e.price}</p>
+
+    <p><strong>Discount:</strong> ${e.discountPercentage}%</p>
+
+    <p><strong>⭐ Rating:</strong> ${e.rating}</p>
+
+    <p><strong>Stock:</strong> ${e.stock}</p>
+
+    <p><strong>Status:</strong> ${e.availabilityStatus}</p>
+
     <button>Buy Now</button>
-  </div>
+</div>
 `
     ))
 }
